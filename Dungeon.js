849dee2e4a2e82195b88ab5cfb9bd7fe3c9eeaaa
VVinsome <you@example.com>
2019-04-19 13:07:30 -0700

    Changed constructor to be factory function and created tests and tested all factory functions

--- Dungeon.js
+++ Dungeon.js
@@ -1,47 +1,89 @@
 //add room as constructor instead of calling new. 
-function Dungeon(){
-    this.roomMap = new Map();
-    this.addVRoom() = function(roomNumber){
-        var newRoom = new Room();
-        this.roomMap.set(roomNumber, newRoom);
+var Dungeon = function () {
+    var roomMap = new Map();
+    var globalChat = [];
+    const addRoomVertex = function (roomNumber, newRoom) {
+        roomMap.set(roomNumber, newRoom);
     }
     /*takes two rooms u, and v and creates edge between them at contrasting
     directions. ie North-South East-West Up-Down
     */
-    this.addRoomEdge = function(u,v,direction){ 
-        if(this.roomMap.has(u) && this.roomMap.has(v)){
-            this.roomMap.get(u).addOutgoingRoom(v, direction);
-            if(direction % 2 == 0){
-                this.roomMap.get(v).addOutgoingRoom(u,direction +1);
+    const addRoomEdge = function (u, v, direction) {
+        if (roomMap.has(u) && roomMap.has(v)) {
+            roomMap.get(u).addOutgoingRoom(v, direction);
+            if (direction % 2 == 0) {
+                roomMap.get(v).addOutgoingRoom(u, direction + 1);
             }
-            else{
-                this.roomMap.get(v).addOutgoingRoom(u,direction -1);
+            else {
+                roomMap.get(v).addOutgoingRoom(u, direction - 1);
             }
 
         }
     }
+    const addGlobalChat = function (message) {
+        globalChat.push(message);
+    }
+    return {
+        get roomMap() {
+            return roomMap;
+        },
+        get globalChat() {
+            return globalChat;
+        },
+        addRoomVertex, addRoomEdge, addGlobalChat
+    };
 };
-var Room = function (){
+var Room = function (roomNum) {
+    var roomNumber = roomNum;
     var peopleInRoom = new Map();
-    var message = [];
-    var outgoingRoom = [null,null,null,null,null,null];
-    const addOutgoingRoom = function(newRoom, direction){
+    var roomChat = [];
+    var outgoingRoom = [null, null, null, null, null, null];
+    const addOutgoingRoom = function (newRoom, direction) {
         outgoingRoom[direction] = newRoom;
     };
-    const addPeople = function(person){
-        peopleInRoom.set(person.getName(),person);
+    const addPeople = function (person) {
+        peopleInRoom.set(person.name, person);
     };
-    const removePeople = function(person){
-        peopleInRoom.delete(person.getName());
+    const removePeople = function (person) {
+        peopleInRoom.delete(person.name);
     };
-    return {peopleInRoom,message,addOutgoingRoom,addPeople,removePeople};
-    
+    const addChat = function (message) {
+        roomChat.push(message);
+    };
+    return {
+        get roomNumber() {
+            return roomNumber;
+        },
+        get peopleInRoom() {
+            return peopleInRoom;
+        },
+        get roomChat() {
+            return roomChat;
+        },
+        get outgoingRoom() {
+            return outgoingRoom;
+        },
+        addOutgoingRoom, addPeople, removePeople, addChat
+    };
+
 };
-var People = function (username){
+var People = function (username) {
     var inbox = [];
     var name = username;
-    const getName = function(){
-        return name;
+    const addMail = function (mail) {
+        inbox.push(mail);
+    }
+    return {
+        get name() {
+            return name;
+        },
+        get inbox() {
+            return inbox;
+        },
+        addMail
+
     };
-    return {inbox,name,getName};
 };
+module.exports.Dungeon = Dungeon;
+module.exports.Room = Room;
+module.exports.People = People;
---
+++ test.js
@@ -0,0 +1,113 @@
+var wholeDungeon = require("./Dungeon.js");
+var dungeon = wholeDungeon.Dungeon;
+var room = wholeDungeon.Room;
+var people = wholeDungeon.People;
+// Testing constructor name
+function testPerson() {
+    var person1 = people("person1");
+    var person2 = people("person2");
+    if (person1.name == "person1") {
+        console.log("person1 initialized name correct");
+    }
+    if (person2.name == "person2") {
+        console.log("person2 initialized name correct");
+        return;
+    }
+    if (person1.name != "person1") {
+        console.log("incorrect name " + person1.name);
+    }
+    if (person2.name != "person2") {
+        console.log("incorrect name " + person1.name);
+    }
+}
+//testing constructor inbox
+function testPersonInbox() {
+    var person1 = people("person1");
+    var person2 = people("person2");
+    person1.addMail("person1Mail");
+    person2.addMail("person2Mail");
+    if (person1.inbox[0] == "person1Mail") {
+        console.log("person1 inbox: " + person1.inbox[0]);
+    }
+    if (person2.inbox[0] == "person2Mail") {
+        console.log("person2 inbox: " + person2.inbox[0]);
+        return;
+    }
+    console.log("person inbox fail, Person1: " + person1.inbox[0] + "person2: " + person2.inbox[0]);
+}
+
+function testRoomNumber() {
+    var room1 = room(1);
+    var room2 = room(2);
+    if (room1.roomNumber == 1 && room2.roomNumber == 2) {
+        console.log("room number correct");
+    }
+    else {
+        console.log("room number incorrect room1: " + room1.roomNumber + " room2: " + room2.roomNumber);
+    }
+}
+/*
+function logMapElements(value, key, map) {
+    console.log(`map.get('${key}') = ${value}`);
+}
+*/
+function testRoomPeople() {
+    var person1 = people("person1");
+    var room1 = room(1);
+    room1.addPeople(person1);
+    if (room1.peopleInRoom.get("person1") == person1) {
+        console.log("room people true");
+    }
+    else {
+        var get_entries = room1.peopleInRoom.entries();
+        var get_keys = room1.peopleInRoom.keys();
+        for (var ele of get_entries) {
+            console.log(ele);
+        }
+        for (var elem of get_keys) {
+            console.log(elem);
+        }
+        for (let elem of room1.peopleInRoom.entries()) {
+            console.log(`${elem[0].name}: ${elem[1]}`);
+        }
+    }
+    room1.addChat("hi");
+    if (room1.roomChat[0] == "hi") {
+        console.log("adding to chat works");
+    }
+    else {
+        console.log("chat doesnt work: " + room1.roomChat);
+    }
+    var room2 = room(2);
+    room1.addOutgoingRoom(room2, 0);
+    if (room1.outgoingRoom[0] === room2) {
+        console.log("room outgoing add works");
+    }
+    else {
+        console.log("room outgoing does not work: " + room1.outgoingRoom);
+    }
+}
+function dungeonTest() {
+    var person1 = people(person1);
+    var room1 = room(1);
+    var room2 = room(2);
+    var dungeon1 = dungeon();
+    dungeon1.addRoomVertex(1, room1);
+    dungeon1.addRoomVertex(2, room2);
+    dungeon1.addRoomEdge(1, 2, 0);
+    if (dungeon1.roomMap.get(1).outgoingRoom[0] == 2) {
+        console.log("room1 has edge to room2 in correct direction");
+    }
+    if (dungeon1.roomMap.get(2).outgoingRoom[1] == 1) {
+        console.log("room2 has edge to room1 in correct direction");
+        return
+    }
+    console.log("dungeon error: room1 in dungeon: " + dungeon1.roomMap.get(1).outgoingRoom[0]);
+    console.log("dungeon error: room2 in dungeon: " + dungeon1.roomMap.get(2).outgoingRoom[1]);
+}
+
+testPerson();
+testPersonInbox();
+testRoomNumber();
+testRoomPeople();
+dungeonTest();
